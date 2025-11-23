2ad4272a5909e7227d716f3e90b86df25b072c11
HariOmPandey07 <05altamash05@gmail.com>
2025-11-23 20:57:23 +0530

    final commit

---
+++ AuthAPI/Controllers/UserAuthController.cs
@@ -0,0 +1,108 @@
+﻿using AuthAPI.Data;
+using AuthAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserAuthController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signinManager;
+        private readonly string? _jwtKey;
+        private readonly string? _jwtIssuer;
+        private readonly string? _jwtAudience;
+        private readonly int _jwtExpiry;
+
+        public UserAuthController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signinManager,
+            IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signinManager = signinManager;
+            _jwtKey = configuration["Jwt:Key"];
+            _jwtIssuer = configuration["Jwt:Issuer"];
+            _jwtAudience = configuration["Jwt:Audience"];
+            _jwtExpiry = configuration.GetValue<int>("Jwt:ExpiryInMinutes");
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel registerModel)
+        {
+            if (registerModel == null || string.IsNullOrEmpty(registerModel.Name) ||
+                string.IsNullOrEmpty(registerModel.Email) || string.IsNullOrEmpty(registerModel.Password))
+            {
+                return BadRequest("Invalid registration details.");
+            } 
+
+            var existingUser = await _userManager.FindByEmailAsync(registerModel.Email);
+            if (existingUser != null)
+            {
+                return Conflict("User with this email already exists.");
+            }
+
+            var user = new ApplicationUser
+            {
+                UserName = registerModel.Email,
+                Email = registerModel.Email,
+                Name = registerModel.Name
+            };
+
+            var result = await _userManager.CreateAsync(user, registerModel.Password);
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+            return Ok("User registered successfully.");
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel loginModel)
+        {
+            var user = await _userManager.FindByEmailAsync(loginModel.Email);
+            if(user == null)
+            {
+                return Unauthorized("Invalid Username or password");
+            }
+            var result = await _signinManager.CheckPasswordSignInAsync(user, loginModel.Password, false);
+
+            if (!result.Succeeded)
+            {
+                return Unauthorized("Invalid Username or password");
+            }
+
+            var token = GenerateJWTToken(user);
+
+            return Ok(new { success = true, token });
+        }
+
+        private string GenerateJWTToken(ApplicationUser user)
+        {
+            var Claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim("Name", user.Name),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtKey));
+
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                claims: Claims,
+                expires: DateTime.Now.AddMinutes(_jwtExpiry),
+                signingCredentials: cred
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ AuthAPI/Program.cs
@@ -0,0 +1,64 @@
+using AuthAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//Identity Configuration
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = false;
+
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 1;
+})
+    .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+//JWT Authentication Configuration
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+   options.TokenValidationParameters = new TokenValidationParameters
+   {
+       ValidateIssuer = false,
+       ValidateAudience = false,
+       ValidateLifetime = true,
+       ValidateIssuerSigningKey = true,
+       ValidIssuer = builder.Configuration["Jwt:Issuer"],
+       ValidAudience = builder.Configuration["Jwt:Audience"],
+       IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+   };
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
